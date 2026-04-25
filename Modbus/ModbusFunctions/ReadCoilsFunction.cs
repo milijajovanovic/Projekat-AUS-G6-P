50a9fecb964a7f2d82e8f5d71a2f9c7bb2e98adb
Milija Jovanović <jovanovichmiki@gmail.com>
2026-04-25 20:53:52 +0200

    Feature/read coils function (#2)
    * Implement Modbus Read Coils function logic

--- Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -2,7 +2,6 @@
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Reflection;
 
 namespace Modbus.ModbusFunctions
@@ -16,7 +15,7 @@ namespace Modbus.ModbusFunctions
         /// Initializes a new instance of the <see cref="ReadCoilsFunction"/> class.
         /// </summary>
         /// <param name="commandParameters">The modbus command parameters.</param>
-		public ReadCoilsFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
+        public ReadCoilsFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
         {
             CheckArguments(MethodBase.GetCurrentMethod(), typeof(ModbusReadCommandParameters));
         }
@@ -24,15 +23,48 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters parameters = CommandParameters as ModbusReadCommandParameters;
+            byte[] requestBytes = new byte[12];
+
+            requestBytes[0] = (byte)(parameters.TransactionId >> 8);
+            requestBytes[1] = (byte)(parameters.TransactionId);
+            requestBytes[2] = 0;
+            requestBytes[3] = 0;
+            requestBytes[4] = 0;
+            requestBytes[5] = 6;
+            requestBytes[6] = parameters.UnitId;
+            requestBytes[7] = parameters.FunctionCode;
+            requestBytes[8] = (byte)(parameters.StartAddress >> 8);
+            requestBytes[9] = (byte)(parameters.StartAddress);
+            requestBytes[10] = (byte)(parameters.Quantity >> 8);
+            requestBytes[11] = (byte)(parameters.Quantity);
+
+            return requestBytes;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters parameters = CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> coilStates = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response == null || response.Length < 9)
+                throw new ArgumentException("Invalid response length.");
+
+            if (response[7] == parameters.FunctionCode + 0x80)
+            {
+                HandeException(response[8]);
+            }
+
+            for (int i = 0; i < parameters.Quantity; i++)
+            {
+                int targetByteIndex = 9 + (i / 8);
+                int targetBitIndex = i % 8;
+                ushort state = (ushort)((response[targetByteIndex] >> targetBitIndex) & 1);
+                coilStates.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, (ushort)(parameters.StartAddress + i)), state);
+            }
+
+            return coilStates;
         }
     }
 }
