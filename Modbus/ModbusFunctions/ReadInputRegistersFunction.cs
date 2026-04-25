6039d7a496d3d1874bec6748259471493838ad6e
Milija <jovanovichmiki@gmail.com>
2026-04-25 21:07:53 +0200

    Implement remaining Modbus functions

--- Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -2,7 +2,6 @@
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Reflection;
 
 namespace Modbus.ModbusFunctions
@@ -24,15 +23,54 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters p = CommandParameters as ModbusReadCommandParameters;
+            byte[] request = new byte[12];
+
+            // Transaction ID
+            request[0] = (byte)(p.TransactionId >> 8);
+            request[1] = (byte)(p.TransactionId);
+            // Protocol ID = 0
+            request[2] = 0;
+            request[3] = 0;
+            // Length = 6
+            request[4] = 0;
+            request[5] = 6;
+            // Unit ID
+            request[6] = p.UnitId;
+            // Function Code
+            request[7] = p.FunctionCode;
+            // Start Address
+            request[8] = (byte)(p.StartAddress >> 8);
+            request[9] = (byte)(p.StartAddress);
+            // Quantity
+            request[10] = (byte)(p.Quantity >> 8);
+            request[11] = (byte)(p.Quantity);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters p = CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response == null || response.Length < 9)
+                throw new ArgumentException("Invalid response length.");
+
+            if (response[7] == p.FunctionCode + 0x80)
+            {
+                HandeException(response[8]);
+            }
+
+            // Podaci počinju od indexa 9, svaki registar je 2 bajta, big endian
+            for (int i = 0; i < p.Quantity; i++)
+            {
+                ushort value = (ushort)((response[9 + 2 * i] << 8) | response[10 + 2 * i]);
+                result.Add(new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, (ushort)(p.StartAddress + i)), value);
+            }
+
+            return result;
         }
     }
 }
--- Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -2,7 +2,6 @@
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Reflection;
 
 namespace Modbus.ModbusFunctions
@@ -24,15 +23,51 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = CommandParameters as ModbusWriteCommandParameters;
+            byte[] request = new byte[12];
+
+            // Transaction ID
+            request[0] = (byte)(p.TransactionId >> 8);
+            request[1] = (byte)(p.TransactionId);
+            // Protocol ID = 0
+            request[2] = 0;
+            request[3] = 0;
+            // Length = 6
+            request[4] = 0;
+            request[5] = 6;
+            // Unit ID
+            request[6] = p.UnitId;
+            // Function Code
+            request[7] = p.FunctionCode;
+            // Output Address
+            request[8] = (byte)(p.OutputAddress >> 8);
+            request[9] = (byte)(p.OutputAddress);
+            // Vrednost: 0xFF00 = ON, 0x0000 = OFF
+            request[10] = (byte)(p.Value == 1 ? 0xFF : 0x00);
+            request[11] = 0x00;
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = CommandParameters as ModbusWriteCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response == null || response.Length < 12)
+                throw new ArgumentException("Invalid response length.");
+
+            if (response[7] == p.FunctionCode + 0x80)
+            {
+                HandeException(response[8]);
+            }
+
+            // Odgovor je echo zahteva, vrednost je na bajtovima 10-11
+            ushort value = (ushort)(response[10] == 0xFF ? 1 : 0);
+            result.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, p.OutputAddress), value);
+
+            return result;
         }
     }
 }
