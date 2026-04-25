65dab2e302cc121478e8a41125f069c34766c136
Milija <jovanovichmiki@gmail.com>
2026-04-25 18:50:03 +0200

    Initial commit

---
+++ ProcessingModule/Acquisitor.cs
@@ -0,0 +1,103 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ProcessingModule
+{
+    /// <summary>
+    /// Class containing logic for periodic polling.
+    /// </summary>
+    public class Acquisitor : IDisposable
+	{
+		private AutoResetEvent acquisitionTrigger;
+        private IProcessingManager processingManager;
+        private Thread acquisitionWorker;
+		private IStateUpdater stateUpdater;
+		private IConfiguration configuration;
+        private Dictionary<string, int> acquisitionCounters = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Acquisitor"/> class.
+        /// </summary>
+        /// <param name="acquisitionTrigger">The acquisition trigger.</param>
+        /// <param name="processingManager">The processing manager.</param>
+        /// <param name="stateUpdater">The state updater.</param>
+        /// <param name="configuration">The configuration.</param>
+		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IStateUpdater stateUpdater, IConfiguration configuration)
+		{
+			this.stateUpdater = stateUpdater;
+			this.acquisitionTrigger = acquisitionTrigger;
+			this.processingManager = processingManager;
+			this.configuration = configuration;
+			this.InitializeAcquisitionThread();
+			this.StartAcquisitionThread();
+		}
+
+		#region Private Methods
+
+        /// <summary>
+        /// Initializes the acquisition thread.
+        /// </summary>
+		private void InitializeAcquisitionThread()
+		{
+			this.acquisitionWorker = new Thread(Acquisition_DoWork);
+			this.acquisitionWorker.Name = "Acquisition thread";
+		}
+
+        /// <summary>
+        /// Starts the acquisition thread.
+        /// </summary>
+		private void StartAcquisitionThread()
+		{
+			acquisitionWorker.Start();
+		}
+
+        /// <summary>
+        /// Acquisitor thread logic.
+        /// </summary>
+		private void Acquisition_DoWork()
+		{
+            // Initialize counters for each configuration item
+            var configItems = configuration.GetConfigurationItems();
+            foreach (var item in configItems)
+            {
+                acquisitionCounters[item.Description] = 0;
+            }
+
+            while (true)
+            {
+                acquisitionTrigger.WaitOne();
+
+                foreach (var configItem in configItems)
+                {
+                    acquisitionCounters[configItem.Description]++;
+                    
+                    // Check if it's time to read this point type
+                    if (acquisitionCounters[configItem.Description] >= configItem.AcquisitionInterval)
+                    {
+                        acquisitionCounters[configItem.Description] = 0;
+                        
+                        // Execute read command for this point type
+                        processingManager.ExecuteReadCommand(
+                            configItem,
+                            configuration.GetTransactionId(),
+                            configuration.UnitAddress,
+                            configItem.StartAddress,
+                            configItem.NumberOfRegisters
+                        );
+                    }
+                }
+            }
+        }
+
+        #endregion Private Methods
+
+        /// <inheritdoc />
+        public void Dispose()
+		{
+			acquisitionWorker.Abort();
+        }
+	}
+}
